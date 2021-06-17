da083db358fd1c730b74001016f58c1e88b0b3cd
Leonard Tng <leonardtng@gmail.com>
2021-06-18 02:19:27 +0800

    Closes #10; Edited heatmap legend

--- src/components/UI/molecules/CorrelationHeatmap.tsx
+++ src/components/UI/molecules/CorrelationHeatmap.tsx
@@ -147,12 +147,12 @@ const CorrelationHeatmap: React.FC = () => {
           },
           {
             from: 70,
-            to: 89.999999999,
-            name: 'High Correlation (<90)',
+            to: 84.999999999,
+            name: 'High Correlation (<85)',
             color: theme.palette.warning.main
           },
           {
-            from: 90,
+            from: 85,
             to: 99.999999999,
             name: 'Extreme Correlation (<99.999)',
             color: theme.palette.error.main
