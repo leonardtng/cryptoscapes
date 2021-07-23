16c0882569b58fac4f4e74174123e4aaa8a2e7af
Leonard Tng <leonardtng@gmail.com>
2021-07-23 15:53:22 +0800

    Fixed a UI bug where illustration was not aligned center on Safari

--- src/components/templates/InProgressLayout.tsx
+++ src/components/templates/InProgressLayout.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Theme, makeStyles } from '@material-ui/core/styles';
 import { Box, Typography } from '@material-ui/core';
 import WorkInProgress from '../../assets/images/illustrations/work-in-progress.svg';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  illustration: { 
+    paddingTop: theme.spacing(3)
+  }
+}));
+
 const InProgressLayout: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100%" width="100%">
       <Typography variant="h5">Stay Tuned!</Typography>
       <Typography variant="body1" color="textSecondary">This page is coming soon</Typography>
-      <Box paddingTop="24px"  height="30%">
-        <img src={WorkInProgress} alt="Work in Progress" height="100%" />
-      </Box>
+      <img src={WorkInProgress} alt="Work in Progress" height="30%" className={classes.illustration} />
     </Box>
   )
 }
